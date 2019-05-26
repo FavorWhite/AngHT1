0cded7c3e88d48bf4742068455e8632042ec4e2b
Valery <Valery_Bilyi@epam.com>
2019-05-26 17:20:08 +0300

    Fixes after ng lint

--- shop/src/app/cart/cart.service.ts
+++ shop/src/app/cart/cart.service.ts
@@ -2,18 +2,18 @@ import { Injectable } from '@angular/core';
 import { Product } from '../common/classes/product';
 import { ProdCategory } from '../common/prodCategory';
 
-let boughtProducts: Product[] = [
+const boughtProducts: Product[] = [
   {
-    id: "b1d2cc08-4144-4758-84d0-83ba008d4a08",
-    name: "Wine - Sauvignon Blanc",
-    description: "vel pede morbi porttitor lorem id ligula suspendisse ornare consequat lectus in est risus auctor sed tristique",
+    id: 'b1d2cc08-4144-4758-84d0-83ba008d4a08',
+    name: 'Wine - Sauvignon Blanc',
+    description: 'vel pede morbi porttitor lorem id ligula suspendisse ornare consequat lectus in est risus auctor sed tristique',
     price: 915.91,
     category: ProdCategory.Tablet,
     isAvaliable: true
   }, {
-    id: "44301afb-681f-4bc7-a224-8e0376ef7010",
-    name: "Bagelers - Cinn / Brown Sugar",
-    description: "sed augue aliquam erat volutpat in congue etiam justo etiam pretium iaculis justo in",
+    id: '44301afb-681f-4bc7-a224-8e0376ef7010',
+    name: 'Bagelers - Cinn / Brown Sugar',
+    description: 'sed augue aliquam erat volutpat in congue etiam justo etiam pretium iaculis justo in',
     price: 1171.12,
     category: ProdCategory.PC,
     isAvaliable: false
